ca8422dd19a141514f935d3b7d290aaf75a00a27
Jenniekibiri <josphatje@gmail.com>
2020-02-04 10:24:53 +0300

    Updates

---
+++ precache-manifest.9ed6d92f80b50c88e9355d6ff0812cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9643c8993e0769462ba1a7a39ab26aa",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "a76aaec645b7309eb62a",
+    "url": "/my-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1fba77d7f6a1246d25a5",
+    "url": "/my-portfolio/static/js/2.7d6da53f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-portfolio/static/js/2.7d6da53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a76aaec645b7309eb62a",
+    "url": "/my-portfolio/static/js/main.06b3989e.chunk.js"
+  },
+  {
+    "revision": "164cd71dc8e56a933ce3",
+    "url": "/my-portfolio/static/js/runtime-main.d96a2d46.js"
+  }
+]);
